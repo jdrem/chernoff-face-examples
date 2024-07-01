913fbcbda3a286b19d4b74d294eff397bb1a7c99
Jeff Remillard <jdr@remgant.net>
2024-07-01 16:23:32 -0400

    Update to java 21, add module info. #5

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module remgant.graphics.chernofffaces.examples {
+    exports net.remgant.graphics.chernoff.examples;
+    requires java.desktop;
+    requires java.xml;
+    requires remgant.graphics.chernofffaces;
+    requires batik.dom;
+    requires batik.svggen;
+    requires xmlgraphics.commons;
+}
